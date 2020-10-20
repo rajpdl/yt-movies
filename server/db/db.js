5834ba117cbb856da460720c2d33cb03b598d732
rajam-coder <rajamucsmdy@gmail.com>
2020-10-20 10:16:27 +0630

    Added Something

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const { Sequelize} = require('sequelize');
 
-var url = 'postgres://postgres:7252@localhost:5432/movies-list' | process.env.DATABASE_URL ;
+var url = 'postgres://postgres:7252@localhost:5432/movies-list' || process.env.DATABASE_URL ;
 
 var sequelize = new Sequelize(url);
 
